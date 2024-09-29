aea6e758bb161dec51e3193ca7f9a5297cff7183
Nathielle Lopes Harka <107863301+nathiellelopesh@users.noreply.github.com>
2024-09-29 15:01:47 -0300

    Update Home.jsx
    Modified footer import

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import './home.css'
 import Header from "../../Components/HeaderHome/Header";
 import Description from '../../Components/Description/Description';
 import Planos from '../../Components/Planos/Planos';
-import Footer from '../../Components/Footer/footer';
+import Footer from '../../Components/Footer/Footer';
 
 export default function Home() {
     return (
